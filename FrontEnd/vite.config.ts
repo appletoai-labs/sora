f93dfe5fa33dec41c6aef3ea394477b827bd7a4c
zaviyaar-irfan <zaviyaar.irfan@gmail.com>
2025-07-17 17:40:17 +0500

    vite plugins added for environment variables

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -2,15 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
+import envCompatible from "vite-plugin-env-compatible"; 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
+  envPrefix: "REACT_APP_", 
   plugins: [
     react(),
+    envCompatible(),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
